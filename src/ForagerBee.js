bc4703bc31d55d359af19534b8c348870d4f95cc
Chris Picato <christopherpicato@gmail.com>
2016-08-12 10:37:07 -0700

    Finishes ES6 refactor

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,18 @@
 import Bee from './Bee'
 
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor (color, food, eat) {
+    super(color, food, eat)
+    this.age = 10
+    this.job = 'find pollen'
+    this.canFly = true
+    this.treasureChest = []
+  }
+
+  forage (treasure) {
+    this.treasureChest.push(treasure)
+  }
 };
 
 export default ForagerBee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,7 +1,21 @@
 import Bee from './Bee'
 
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
   // TODO..
+  constructor (job, color, food, eat) {
+    super(job, color, food, eat)
+    this.age = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+
+  makeHoney () {
+    this.honeyPot++
+  }
+
+  giveHoney () {
+    this.honeyPot--
+  }
 };
 
 export default HoneyMakerBee;
